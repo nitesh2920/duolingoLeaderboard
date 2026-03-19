5ba8e304efea5c2a305f53212f8b2e48347ee43b
nitesh2920 <niteshtiwari0234@gmail.com>
2026-03-19 16:33:39 +0530

    feat: mockdata

---
+++ data/mockUsers.ts
@@ -0,0 +1,13 @@
+import { User } from "@/types/leaderboard";
+
+export const currentUserId = "user-1";
+
+export const mockUsers: User[] = [
+  { id: "user-8", username: "LÊ TRƯỜNG GIANG", xp: 12 },
+  { id: "user-9", username: "Haki", xp: 12 },
+  { id: "user-10", username: "PRANAV PRASHANT", xp: 10 },
+  { id: "user-11", username: "Кобальт", xp: 9 },
+  { id: "user-12", username: "ngocmai", xp: 7, avatarUrl: "https://i.pravatar.cc/150?u=a042581f4e29026704d" },
+  { id: "user-13", username: "MALLARAPU YASWANTH", xp: 6 },
+  { id: "user-1", username: "Chirag Maheshwari", xp: 5, avatarUrl: "https://i.pravatar.cc/150?u=a042581f4e29026024d" }, // current user
+];
